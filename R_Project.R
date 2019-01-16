0005a2d15c6022bf0408ab3e2b97a3553ec4d61e
justsocatherine <kathrin.kunfermann@gmail.com>
2019-01-16 11:48:39 +0100

    save

---
+++ R_Project.R
@@ -0,0 +1,8 @@
+#load Datasets
+library(tidyverse)
+library(readr)
+All_waves_GENERAL <- read_csv("C:/Users/kkunfermann/Desktop/R course/All waves GENERAL.csv")
+View(All_waves_GENERAL)
+library(readr)
+Dataset_MCA_EPE_2014_NL_FINAL <- read_csv("C:/Users/kkunfermann/Desktop/R course/Dataset MCA EPE 2014 NL FINAL.csv")
+View(Dataset_MCA_EPE_2014_NL_FINAL)
