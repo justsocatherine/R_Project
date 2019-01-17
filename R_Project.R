6d0ee969e8a45f85afc3540f742d8f713e6b1bea
ZBauman <matteo.rinaldi.91gmail.com>
2019-01-17 12:47:36 +0100

    hi

--- R_Project.R
+++ R_Project.R
@@ -7,3 +7,4 @@ library(readr)
 Dataset_MCA_EPE_2014_NL_FINAL <- read_csv("C:/Users/kkunfermann/Desktop/R course/Dataset MCA EPE 2014 NL FINAL.csv")
 View(Dataset_MCA_EPE_2014_NL_FINAL)
 object <- 12345*97765
+hi
