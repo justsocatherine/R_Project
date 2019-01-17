1db8e6cf7ffd120cd7d520fa40561e4f32f42605
ZBauman <matteo.rinaldi.91gmail.com>
2019-01-17 12:45:37 +0100

    hi

--- R_Project.R
+++ R_Project.R
@@ -6,3 +6,4 @@ View(All_waves_GENERAL)
 library(readr)
 Dataset_MCA_EPE_2014_NL_FINAL <- read_csv("C:/Users/kkunfermann/Desktop/R course/Dataset MCA EPE 2014 NL FINAL.csv")
 View(Dataset_MCA_EPE_2014_NL_FINAL)
+object <- 12345*97765
